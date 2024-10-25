6abb403e1b04555c61d143fc2c0035d35cad6537
lxishh <henrix913@gmail.com>
2024-10-25 15:03:07 -0300

    /admin

--- appAlumno/admin.py
+++ appAlumno/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from appAlumno.models import Alumno
 
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ['dni', 'direccion', 'telefono', 'edad']
+
 # Register your models here.
-admin.site.register(Alumno)
+admin.site.register(Alumno, AlumnoAdmin)
